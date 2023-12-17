9b54c93dbcb61fc50807f6b46ec13566ab7c125f
Aman-kinger <amankinger01@gmail.com>
2023-12-18 01:55:56 +0530

    extract urls from html

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 function normalizeURL(url){
     url = new URL(url);
     let host = url.hostname;
@@ -12,9 +15,24 @@ function normalizeURL(url){
     return new_url;
 }
 
-console.log(normalizeURL(new URL("https://www.google.com")))
+function getURLSFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody);
+    let urls = [];
+    let links = dom.window.document.querySelectorAll("a");
+    for (let i = 0; i < links.length; i++) {
+        let url = links[i].href;
+        if (url.startsWith(baseURL)) {
+            urls.push(url);
+        }
+        else if (url.startsWith("/")){
+            urls.push(baseURL + url);
+        }
+    }
+    return urls;
+}
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLSFromHTML
   }
   
--- crawl.test.js
+++ crawl.test.js
@@ -1,34 +1,72 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+const { JSDOM } = require('jsdom');
+const { getURLSFromHTML } = require('./crawl.js');
 
-test('normalizeURL with standard URL', () => {
-    expect(normalizeURL('https://www.google.com')).toBe('www.google.com');
+
+describe('normalizeURL tests', () => {
+    test('normalizeURL with standard URL', () => {
+        expect(normalizeURL('https://www.google.com')).toBe('www.google.com');
+    });
+
+    test('normalizeURL with trailing slash', () => {
+        expect(normalizeURL('https://www.google.com/')).toBe('www.google.com');
+    });
+
+    test('normalizeURL with path', () => {
+        expect(normalizeURL('https://www.google.com/search')).toBe('www.google.com/search');
+    });
+
+    test('normalizeURL with path and trailing slash', () => {
+        expect(normalizeURL('https://www.google.com/search/')).toBe('www.google.com/search');
+    });
+
+    test('normalizeURL with subdomain', () => {
+        expect(normalizeURL('https://blog.google.com')).toBe('blog.google.com');
+    });
+
+    test('normalizeURL with HTTP protocol', () => {
+        expect(normalizeURL('http://www.google.com')).toBe('www.google.com');
+    });
+
+    test('normalizeURL without protocol', () => {
+        expect(normalizeURL('www.google.com')).toBe('www.google.com');
+    });
+
+    test('normalizeURL with IP address', () => {
+        expect(normalizeURL('https://192.168.1.1')).toBe('192.168.1.1');
+    });
 });
 
-test('normalizeURL with trailing slash', () => {
-    expect(normalizeURL('https://www.google.com/')).toBe('www.google.com');
-});
 
-test('normalizeURL with path', () => {
-    expect(normalizeURL('https://www.google.com/search')).toBe('www.google.com/search');
-});
+describe('getURLSFromHTML tests', () => {
+    test('getURLSFromHTML with no links', () => {
+        const htmlBody = '<html><body></body></html>';
+        const baseURL = 'https://www.google.com';
+        expect(getURLSFromHTML(htmlBody, baseURL)).toEqual([]);
+    });
 
-test('normalizeURL with path and trailing slash', () => {
-    expect(normalizeURL('https://www.google.com/search/')).toBe('www.google.com/search');
-});
+    test('getURLSFromHTML with one absolute link', () => {
+        const htmlBody = '<html><body><a href="https://www.google.com/search"></a></body></html>';
+        const baseURL = 'https://www.google.com';
+        expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(['https://www.google.com/search']);
+    });
 
-test('normalizeURL with subdomain', () => {
-    expect(normalizeURL('https://blog.google.com')).toBe('blog.google.com');
-});
+    test('getURLSFromHTML with one relative link', () => {
+        const htmlBody = '<html><body><a href="/search"></a></body></html>';
+        const baseURL = 'https://www.google.com';
+        expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(['https://www.google.com/search']);
+    });
 
-test('normalizeURL with HTTP protocol', () => {
-    expect(normalizeURL('http://www.google.com')).toBe('www.google.com');
-});
+    test('getURLSFromHTML with multiple links', () => {
+        const htmlBody = '<html><body><a href="https://www.google.com/search"></a><a href="/images"></a></body></html>';
+        const baseURL = 'https://www.google.com';
+        expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(['https://www.google.com/search', 'https://www.google.com/images']);
+    });
 
-test('normalizeURL without protocol', () => {
-    expect(normalizeURL('www.google.com')).toBe('www.google.com');
-});
-
-test('normalizeURL with IP address', () => {
-    expect(normalizeURL('https://192.168.1.1')).toBe('192.168.1.1');
+    test('getURLSFromHTML with non-matching links', () => {
+        const htmlBody = '<html><body><a href="https://www.google.com/search"></a><a href="https://www.bing.com"></a></body></html>';
+        const baseURL = 'https://www.google.com';
+        expect(getURLSFromHTML(htmlBody, baseURL)).toEqual(['https://www.google.com/search']);
+    });
 });
